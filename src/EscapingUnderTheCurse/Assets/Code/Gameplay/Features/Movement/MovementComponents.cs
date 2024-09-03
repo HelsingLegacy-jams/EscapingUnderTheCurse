845a8a8fe69617dddf8c622bf2e1e32332ea19c0
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-09-02 22:15:10 -0400

    Jumping components and system created.

--- src/EscapingUnderTheCurse/Assets/Code/Gameplay/Features/Movement/MovementComponents.cs
+++ src/EscapingUnderTheCurse/Assets/Code/Gameplay/Features/Movement/MovementComponents.cs
@@ -3,9 +3,11 @@
 namespace Code.Gameplay.Features.Movement
 {
   [Game] public class Moving : IComponent {}
+  [Game] public class Jumping : IComponent {}
   [Game] public class MovementAvailable : IComponent {}
   
   [Game] public class Speed : IComponent { public float Value; }
+  [Game] public class JumpingForce : IComponent { public float Value; }
   [Game] public class Direction : IComponent { public MovingDirection Value; }
   [Game] public class DirectionModifier : IComponent { public int Value; }
 }
--- src/EscapingUnderTheCurse/Assets/Code/Generated/Game/GameComponentsLookup.cs
+++ src/EscapingUnderTheCurse/Assets/Code/Generated/Game/GameComponentsLookup.cs
@@ -18,11 +18,13 @@ public static class GameComponentsLookup {
     public const int Hero = 7;
     public const int Direction = 8;
     public const int DirectionModifier = 9;
-    public const int MovementAvailable = 10;
-    public const int Moving = 11;
-    public const int Speed = 12;
+    public const int Jumping = 10;
+    public const int JumpingForce = 11;
+    public const int MovementAvailable = 12;
+    public const int Moving = 13;
+    public const int Speed = 14;
 
-    public const int TotalComponents = 13;
+    public const int TotalComponents = 15;
 
     public static readonly string[] componentNames = {
         "View",
@@ -35,6 +37,8 @@ public static class GameComponentsLookup {
         "Hero",
         "Direction",
         "DirectionModifier",
+        "Jumping",
+        "JumpingForce",
         "MovementAvailable",
         "Moving",
         "Speed"
@@ -51,6 +55,8 @@ public static class GameComponentsLookup {
         typeof(Code.Gameplay.Features.Hero.Hero),
         typeof(Code.Gameplay.Features.Movement.Direction),
         typeof(Code.Gameplay.Features.Movement.DirectionModifier),
+        typeof(Code.Gameplay.Features.Movement.Jumping),
+        typeof(Code.Gameplay.Features.Movement.JumpingForce),
         typeof(Code.Gameplay.Features.Movement.MovementAvailable),
         typeof(Code.Gameplay.Features.Movement.Moving),
         typeof(Code.Gameplay.Features.Movement.Speed)
