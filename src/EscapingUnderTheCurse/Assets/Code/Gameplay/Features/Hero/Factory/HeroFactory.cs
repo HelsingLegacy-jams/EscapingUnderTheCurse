3cb6f21227938cf5842b73d8295feaea6963e33b
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-09-02 22:15:03 -0400

    Hero Factory refactored

--- src/EscapingUnderTheCurse/Assets/Code/Gameplay/Features/Hero/Factory/HeroFactory.cs
+++ src/EscapingUnderTheCurse/Assets/Code/Gameplay/Features/Hero/Factory/HeroFactory.cs
@@ -1,19 +1,31 @@
 ﻿using Code.Common;
 using Code.Common.Entity;
+using Code.Gameplay.Configs;
 using Code.Gameplay.Features.Movement;
+using Code.Infrastructure.StaticData;
 using UnityEngine;
 
 namespace Code.Gameplay.Features.Hero.Factory
 {
   public class HeroFactory : IHeroFactory
   {
+    private readonly IStaticDataService _staticData;
+
+    public HeroFactory(IStaticDataService staticData)
+    {
+      _staticData = staticData;
+    }
+
     public GameEntity CreateHero(Vector2 at)
     {
+      HeroStats config = _staticData.HeroConfig;
+      
       return CreateEntity.Empty()
           .AddId(1)
           .AddViewPath("Hero/Hero")
           .AddWorldPosition(at)
-          .AddSpeed(500)
+          .AddSpeed(config.Speed)
+          .AddJumpingForce(config.JumpingForce)
           .AddDirection(MovingDirection.Right)
           .With(x=>x.isHero = true)
           .With(x=>x.isMovementAvailable = true)
